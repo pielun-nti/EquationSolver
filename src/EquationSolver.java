24f96a7396400776c2d6532357442a0abeabdf08
Pierre Lundström <pierre.lundstrom@learnet.se>
2019-10-24 11:11:15 +0200

    second commit

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -8,14 +8,30 @@ import javax.swing.*;
 
 public class EquationSolver {
     public static void main(String[] args) {
-        int n;
-        int k = 1;
-        int amount = 0;
-        n = Integer.parseInt(JOptionPane.showInputDialog("n?"));
-        while(k <= n) {
+        String equationorder;
+        double a;
+        double x;
+        double b;
+        double c;
+        double amount = 0;
+        a = Double.parseDouble(JOptionPane.showInputDialog("a?"));
+        b = Double.parseDouble(JOptionPane.showInputDialog("b?"));
+        equationorder = "ax^2 + b + c = 0";
+        if (equationorder == "ax + b = 0") {
+            x = 0 - b;
+            x = x / a;
+            x = x;
+            JOptionPane.showMessageDialog(null, x);
+        } else if (equationorder == "ax^2 + b + c = 0") {
+            c = Double.parseDouble(JOptionPane.showInputDialog("c?"));
+
+        } else
+        {
+            JOptionPane.showMessageDialog(null, "not ax + b");
+        }
+/*/        while(k <= n) {
             amount = amount + k;
             k = k + 1;
-        }
-        JOptionPane.showMessageDialog(null, "Amount is now " + amount);
+        } /*/
     }
 }
